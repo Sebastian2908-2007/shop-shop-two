d257b27e34ab99b08b8b4e41c4f3bc6b48d81112
Sebabin <sebastiancrespin279@gmail.com>
2022-04-27 13:43:01 -0600

    have a global store object set up with ContextAPI have swiitched the home component from heavvy prop drilling to none at all

--- client/src/components/ProductList/index.js
+++ client/src/components/ProductList/index.js
@@ -1,29 +1,48 @@
-import React from 'react';
+import React ,{ useEffect }from 'react';
 import { useQuery } from '@apollo/client';
+import { UPDATE_PRODUCTS } from '../../utils/actions';
+// import our context hook that includes useContext
+import { useStoreContext } from '../../utils/GlobalState';
 
 import ProductItem from '../ProductItem';
 import { QUERY_PRODUCTS } from '../../utils/queries';
 import spinner from '../../assets/spinner.gif';
 
-function ProductList({ currentCategory }) {
+function ProductList() {
+  // use the useStoreContext function to get global state and dispatch function for changing data
+  const [state, dispatch] = useStoreContext();
+  // destructure currenCategory form the state variable retuned by useStoreContext
+  const { currentCategory } = state;
+  // query for our db data using the useQuery() hook from apollo/client
   const { loading, data } = useQuery(QUERY_PRODUCTS);
 
-  const products = data?.products || [];
+  // use the useEffect hook to update products array
+  useEffect(() => {
+    // useStoreContext will run again after updating the globalState it wil then give us the data neede to display products to the page
+   if (data) {
+     dispatch({
+       type: UPDATE_PRODUCTS,
+       products: data.products
+     });
+   }
+  }, [data, dispatch]);
+
+
 
   function filterProducts() {
     if (!currentCategory) {
-      return products;
+      return state.products;
     }
 
-    return products.filter(
-      (product) => product.category._id === currentCategory
+    return state.products.filter(
+      product => product.category._id === currentCategory
     );
   }
 
   return (
     <div className="my-2">
       <h2>Our Products:</h2>
-      {products.length ? (
+      {state.products.length ? (
         <div className="flex-row">
           {filterProducts().map((product) => (
             <ProductItem
--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -1,25 +1,42 @@
 import React, { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
-
+import { useStoreContext } from '../utils/GlobalState';
+import { UPDATE_PRODUCTS } from '../utils/actions';
 import { QUERY_PRODUCTS } from '../utils/queries';
 import spinner from '../assets/spinner.gif';
 
 function Detail() {
+  // bring in function that holds useContext
+  const [state,dispatch] = useStoreContext();
+  // this comes from react-router-dom and will allow us to use the id we have put in url
   const { id } = useParams();
-
+  // initialize state to an empty object
   const [currentProduct, setCurrentProduct] = useState({});
-
+  // query the graphql db to get all product info
   const { loading, data } = useQuery(QUERY_PRODUCTS);
+  // destructure "products" from our global state
+  const { products } = state;
 
-  const products = data?.products || [];
-
+  // this use effect will run anytime the dependencies change it is initialized by the changing
+  // of the data dependency in this case which will initially be undefined untill the query completes
   useEffect(() => {
+    // if there is products set current product to equal the product with the id we got from useParams()
     if (products.length) {
-      setCurrentProduct(products.find((product) => product._id === id));
+     setCurrentProduct( products.find((product) => product._id === id)
+     )
+    
+    }else if (data) {
+      // if there is data update our globalStore products array
+      dispatch({
+        type: UPDATE_PRODUCTS,
+        products: data.products
+      });
     }
-  }, [products, id]);
+    // dependency array
+  }, [products, data, dispatch, id]);
 
+  // use the data in the currentProduct variable set by our use state functions setter setCurrentProduct();
   return (
     <>
       {currentProduct ? (
