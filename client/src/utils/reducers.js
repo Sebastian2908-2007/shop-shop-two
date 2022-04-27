f58cc9699bc428444493c6fd403d7af8db359abb
Sebabin <sebastiancrespin279@gmail.com>
2022-04-26 21:43:42 -0600

    added global state f unctionality to the app and to the category componet

--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -1,3 +1,6 @@
+// import use reducer from react
+import { useReducer } from 'react';
+
 import {
     UPDATE_PRODUCTS,
     UPDATE_CATEGORIES,
@@ -12,6 +15,18 @@ export const reducer = (state, action) => {
                  ...state,
                  products: [...action.products],
              };
+             // if action type value is the value of `UPDATE_CATEGORIES`, return a new state object with an updated categories array
+             case UPDATE_CATEGORIES:
+                 return {
+                     ...state,
+                     categories: [...action.categories]
+                 }; 
+
+                 case UPDATE_CURRENT_CATEGORY:
+                     return {
+                        ...state,
+                        currentCategory: action.currentCategory
+                     };
 
              // if it's none of these actions, do not update state at all and keep things the same! 
              default:
@@ -19,3 +34,9 @@ export const reducer = (state, action) => {
      }
 };
 
+// This function, useProductReducer(), will be used to help initialize our global state object and then
+// provide us with the functionality for updating that state by automatically running it through
+// our custom reducer() function. Think of this as a more in-depth way of using the useState() Hook
+export function useProductReducer(initialSate) {
+    return useReducer(reducer, initialSate);
+}
