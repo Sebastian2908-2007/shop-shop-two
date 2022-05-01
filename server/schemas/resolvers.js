ea1ed3ce17d8f89a0d991421b08f743f7cc94503
Sebabin <sebastiancrespin279@gmail.com>
2022-04-30 18:12:38 -0600

    added stripe have the checkout page routing and showing cart items properly

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,6 +1,8 @@
 const { AuthenticationError } = require('apollo-server-express');
 const { User, Product, Category, Order } = require('../models');
 const { signToken } = require('../utils/auth');
+// bring stripe into app with a test key generally the key would be included in a .env file
+const stripe = require('stripe')('sk_test_4eC39HqLyjWDarjtT1zdp7dc');
 
 const resolvers = {
   Query: {
@@ -50,6 +52,44 @@ const resolvers = {
       }
 
       throw new AuthenticationError('Not logged in');
+    },
+    checkout: async (parent,args, context) => {
+      // initialize empty line items array
+      const line_items = [];
+      const order = new Order({ products: args.products });
+      const { products } = await order.populate('products').execPopulate();
+      // add for loop to generate stripe products and id's as well as adding them to the line_items array
+      for (let i = 0; i < products.length; i++) {
+        // generate product id
+        const product = await stripe.products.create({
+          name: products[i].name,
+
+          description: products[i].description
+        });
+
+        // generate price id using the product id
+        const price = await stripe.prices.create({
+          product: product.id,
+          unit_amount: products[i].price * 100,
+          currency: 'usd',
+        });
+
+        // add price id to the line items array
+        line_items.push({
+          price: price.id,
+          quantity: 1
+        });
+
+      }
+      // create a checkout session for stripe
+      const session = await stripe.checkout.sessions.create({
+        payment_method_types: ['card'],
+        line_items,
+        mode: 'payment',
+        success_url: 'https://example.com/success?session_id={CHECKOUT_SESSION_ID}',
+        cancel_url: 'https://example.com/cancel'
+      });
+      return { session: session.id };
     }
   },
   Mutation: {
